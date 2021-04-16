3c2f458d6f7cbb42b98538c8ab516570a09565c7
jaseem1999 <65609243+jaseem1999@users.noreply.github.com>
2021-04-16 02:05:32 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 function background(col){
             document.getElementById("screen").style.background=col
-            document.getElementById("ftrTwitter").style.background=col
+            
         }
+function bgcolorfooter(col){
+      document.getElementById("hap").style.color=col;
+}
+
+function singing(col){
+    document.getElementById("screen-1").style.background=col;
+
+}
